ff52edd1f7dbe394c2173d25693cc1f81d188ed7
mfa077 <miftahf77@gmail.com>
2018-12-20 17:41:09 +0700

    Smart Home PIS

--- main_rev2.py
+++ main_rev2.py
@@ -99,23 +99,25 @@ connected=False
 ser_to_hmi='null'
 buff=''
 locations=['/dev/ttyAMA0']
+locations=['serial0']
 ## COM Port settings
 for device in locations: 
     try:
         print ("Trying...",device)
         ## Serial Initialization
         ser_to_hmi = serial.Serial(device,      #port
-                            115200,              #baudrate
-                            serial.EIGHTBITS,   #bytesize
-                            serial.PARITY_ODD,  #parity
-                            serial.STOPBITS_ONE,#stop bit
-                            0,                  #timeout
-                            False,              #xonxoff
-                            False,              #rtscts
-                            0,                  #write_timeout
-                            False,              #dsrdtr
-                            None,               #inter byte timeout
-                            None                #exclusive
+                            115200
+                            # ,              #baudrate
+                            # serial.EIGHTBITS,   #bytesize
+                            # serial.PARITY_ODD,  #parity
+                            # serial.STOPBITS_ONE,#stop bit
+                            # 0,                  #timeout
+                            # False,              #xonxoff
+                            # False,              #rtscts
+                            # 0,                  #write_timeout
+                            # False,              #dsrdtr
+                            # None,               #inter byte timeout
+                            # None                #exclusive
                             )
         connected=True
         break
